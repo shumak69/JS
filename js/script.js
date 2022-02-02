027f81f0b795f946804ce0b27149c1612deb5305
Dmitriy <dima06444@gmail.com>
2022-02-03 00:59:57 +0200

    2.0

--- js/script.js
+++ js/script.js
@@ -9,12 +9,60 @@ const personalMovideDB = {
     privat: false
 };
 
-const question1 = prompt('Один из последних просмотреных фильмов?','');
-const question2 = prompt('На сколько оцените его?','');
-const question3 = prompt('Один из последних просмотреных фильмов?','');
-const question4 = prompt('На сколько оцените его?','');
+if(personalMovideDB.count < 10 && personalMovideDB.count >= 1) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovideDB.count < 30 && personalMovideDB.count >= 10){
+    alert("Вы классический зритель");
+    
+} else if (personalMovideDB.count >= 30 && personalMovideDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
-personalMovideDB.movies[question1] = question2;
-personalMovideDB.movies[question3] = question4;
+// for (let i = 1; i <= 2; i++) {
+//     const question1 = prompt('Один из последних просмотреных фильмов?',''),
+//         question2 = prompt('На сколько оцените его?','');
+//     if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
+//         personalMovideDB.movies[question1] = question2;
+//         console.log('done');
+//     }   else{
+//         console.log('error');
+//         i--;
+//     }
+    
+// }
+
+let i = 1;
+
+// while (i <= 2) {
+//     const question1 = prompt('Один из последних просмотреных фильмов?',''),
+//         question2 = prompt('На сколько оцените его?','');
+//     if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
+//         personalMovideDB.movies[question1] = question2;
+//         console.log('done');
+//     }   else{
+//         console.log('error');
+//         continue;
+//     }
+//     i++;
+// }
+
+do {
+    i++;
+    const question1 = prompt('Один из последних просмотреных фильмов?',''),
+        question2 = prompt('На сколько оцените его?','');
+    if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
+        personalMovideDB.movies[question1] = question2;
+        console.log('done');
+    }   else{
+        console.log('error');
+        i--;
+        continue;
+    }
+    
+}
+while(i <= 2);
 
 console.log(personalMovideDB);
+
