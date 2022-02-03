3bf88b21bb8a1e3e8129b23bb3024753ac9a8788
Dmitriy <dima06444@gmail.com>
2022-02-03 23:04:49 +0200

    3.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilmls = +prompt('Сколько фильмос вы уже посмотрели','');
+let numberOfFilmls;
+
+function start() {
+    numberOfFilmls = +prompt('Сколько фильмос вы уже посмотрели','');
+    while(numberOfFilmls == '' || numberOfFilmls == null || isNaN(numberOfFilmls)) {
+        numberOfFilmls = +prompt('Сколько фильмос вы уже посмотрели','');
+    }
+}
+
+start();
+
 const personalMovideDB = {
     count: numberOfFilmls,
     movies: {},
@@ -9,31 +19,41 @@ const personalMovideDB = {
     privat: false
 };
 
-if(personalMovideDB.count < 10 && personalMovideDB.count >= 1) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovideDB.count < 30 && personalMovideDB.count >= 10){
-    alert("Вы классический зритель");
-    
-} else if (personalMovideDB.count >= 30 && personalMovideDB.count >= 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
+
+
+function detectPersonalLevel() {
+    if(personalMovideDB.count < 10 && personalMovideDB.count >= 1) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovideDB.count < 30 && personalMovideDB.count >= 10){
+        alert("Вы классический зритель");
+        
+    } else if (personalMovideDB.count >= 30 && personalMovideDB.count >= 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
 }
 
-// for (let i = 1; i <= 2; i++) {
-//     const question1 = prompt('Один из последних просмотреных фильмов?',''),
-//         question2 = prompt('На сколько оцените его?','');
-//     if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
-//         personalMovideDB.movies[question1] = question2;
-//         console.log('done');
-//     }   else{
-//         console.log('error');
-//         i--;
-//     }
-    
-// }
+detectPersonalLevel();
 
-let i = 1;
+function rememberMyFilms() {
+    for (let i = 1; i <= 2; i++) {
+        const question1 = prompt('Один из последних просмотреных фильмов?',''),
+            question2 = prompt('На сколько оцените его?','');
+        if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
+            personalMovideDB.movies[question1] = question2;
+            console.log('done');
+        }   else{
+            console.log('error');
+            i--;
+        }
+        
+    }
+}
+
+rememberMyFilms();
+
+// let i = 1;
 
 // while (i <= 2) {
 //     const question1 = prompt('Один из последних просмотреных фильмов?',''),
@@ -48,21 +68,33 @@ let i = 1;
 //     i++;
 // }
 
-do {
-    i++;
-    const question1 = prompt('Один из последних просмотреных фильмов?',''),
-        question2 = prompt('На сколько оцените его?','');
-    if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
-        personalMovideDB.movies[question1] = question2;
-        console.log('done');
-    }   else{
-        console.log('error');
-        i--;
-        continue;
-    }
+// do {
+//     i++;
+//     const question1 = prompt('Один из последних просмотреных фильмов?',''),
+//         question2 = prompt('На сколько оцените его?','');
+//     if (question1 != null && question2 != null && question1 != '' && question2 != '' && question1.length < 50) {
+//         personalMovideDB.movies[question1] = question2;
+//         console.log('done');
+//     }   else{
+//         console.log('error');
+//         i--;
+//         continue;
+//     }
     
+// }
+// while(i <= 2);
+function showMyDB(privat) {
+    if(!privat) {
+        console.log(personalMovideDB);
+    }
 }
-while(i <= 2);
 
-console.log(personalMovideDB);
+showMyDB(personalMovideDB.privat);
 
+function writeYourGenres() {
+    for (let i = 0;i < 3; i++) {
+        personalMovideDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+    }
+}
+
+writeYourGenres();
