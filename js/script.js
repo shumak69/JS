8792ff94168639d1a99b0d529ebc1659962e013b
Dmitriy <dima06444@gmail.com>
2022-02-10 20:33:55 +0200

    8.0

--- js/script.js
+++ js/script.js
@@ -91,6 +91,65 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     } 
     setClock('.timer', deadline);
-});
 
-const now = new Date();
+    // Modal
+
+    let checkModal = true;
+
+    const modalOpen = document.querySelectorAll('[data-modal]'),
+        modalClose = document.querySelector('[data-close]'),
+        modal = document.querySelector('.modal');
+
+
+    modalOpen.forEach(item => {
+        item.addEventListener('click', () => {
+            showModal();
+        });
+    });
+    modal.style.display = 'block';
+    modal.style.opacity = 0;
+    modal.style.visibility = 'hidden';
+    function showModal() {
+        modal.style.transition = '.5s';
+        modal.style.opacity = 1;
+        // modal.style.display = 'block';
+        modal.style.visibility = 'visible';
+        document.body.style.overflow = 'hidden';
+        checkModal = false;
+        window.removeEventListener('scroll', showModalByScroll);
+        clearInterval(modalTime);
+    }
+
+    function closeModal() {
+        modal.style.opacity = 0;
+        modal.style.visibility = 'hidden';
+        document.body.style.overflow = 'auto';
+    }
+
+    modalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', e => {
+        if(e.code === "Escape" && modal.style.display == 'block') {
+            closeModal();
+        }
+    });
+
+    function showModalByScroll() {
+        if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+            showModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    const modalTime = setTimeout( showModal, 5 * 1000);
+    if(checkModal) {
+        window.addEventListener('scroll', showModalByScroll);
+    }
+    
+});
